075687a3f5568eedc010189d93bb46b1d8fdafb8
hongorzulnemo <lolhongorzul@gmail.com>
2020-03-17 18:13:15 +0100

    main finished

---
+++ src/init-firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyDPyFN9_u9-FqfWpQAUehftoDHLm9YprZI",
+  authDomain: "writeonme-c4042.firebaseapp.com",
+  databaseURL: "https://writeonme-c4042.firebaseio.com",
+  projectId: "writeonme",
+  storageBucket: "writeonme.appspot.com",
+  messagingSenderId: "271318331074",
+  appId: "1:271318331074:web:c7cf9416a2306fb1d7d55b"
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
