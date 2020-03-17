62a91f3a77cc807eb097fde7b8aa46c1b374f6fc
hongorzulnemo <lolhongorzul@gmail.com>
2020-03-17 18:13:15 +0100

    main finished

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
     <Router>
         <App />
+        {/* <!-- The core Firebase JS SDK is always required and must be listed first --> */}
+        <script defer src="/__/firebase/7.11.0/firebase-app.js"></script>
+
+        {/* <!-- TODO: Add SDKs for Firebase products that you want to use
+        https://firebase.google.com/docs/web/setup#available-libraries -->
+
+        <!-- Initialize Firebase --> */}
+        <script defer src="/__/firebase/init.js"></script>
+        <script defer src="./init-firebase.js"></script>
     </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
