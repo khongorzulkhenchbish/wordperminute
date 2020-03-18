0763dbc5116a0acccb2656d022a30c18c476e929
hongorzulnemo <lolhongorzul@gmail.com>
2020-03-18 23:42:19 +0100

    test video

--- src/components/Adv.js
+++ src/components/Adv.js
@@ -5,16 +5,11 @@ const Adv = () => {
     return (
         <Row className="justify-content-center d-flex adv">
           <div className="contentBox text-right">
-            <iframe width="560" height="315" title="video" src="https://www.youtube.com/embed/v4xZUr0BEfE" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/GIsc6Q9VkcA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
           <div className="contentBox">
-            <h1>BROUGHT TO YOU BY LIVECHAT</h1>
-            <p>LiveChat is an application that enables the visitors on your site to 
-              <strong>ch
-              at live with your customer support</strong>. As opposed 
-            to the outdated phone technology, LiveChat lets you to chat with severa
-            l customers at the same time, track their behaviour and measure sales conversio
-            n. Give us a try!</p>
+            <h1>ADVERTISEMENT GOES HERE</h1>
+            <p>Sed ut perspiciatis laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
             <button type="button" className="btn btn-primary" >SIGN UP FOR FREE</button>
           </div>
         </Row>
