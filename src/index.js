cddba45543f13ad937d1b613c99cb8cb85de2bae
hongorzulnemo <lolhongorzul@gmail.com>
2020-03-21 13:47:53 +0100

    bug fix

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ ReactDOM.render(
     <Router>
         <App />
         {/* <!-- The core Firebase JS SDK is always required and must be listed first --> */}
-        <script defer src="/__/firebase/7.11.0/firebase-app.js"></script>
+        <script defer src="/__/firebase/7.12.0/firebase-app.js"></script>
 
         {/* <!-- TODO: Add SDKs for Firebase products that you want to use
         https://firebase.google.com/docs/web/setup#available-libraries -->
 
         <!-- Initialize Firebase --> */}
         <script defer src="/__/firebase/init.js"></script>
-        <script defer src="./init-firebase.js"></script>
+        <script defer src="init-firebase.js"></script>
     </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
