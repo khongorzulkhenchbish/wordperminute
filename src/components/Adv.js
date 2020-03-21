c00fe08996065853dc923bd2a42a175cb49302c7
hongorzulnemo <lolhongorzul@gmail.com>
2020-03-21 13:55:25 +0100

    unnecessary file removed

--- src/components/Adv.js
+++ src/components/Adv.js
@@ -9,7 +9,7 @@ const Adv = () => {
           </div>
           <div className="contentBox">
             <h1>ADVERTISEMENT GOES HERE</h1>
-            <p>Sed ut perspiciatis laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+            <p>Sed perspiciatis laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
             <button type="button" className="btn btn-primary" >SIGN UP FOR FREE</button>
           </div>
         </Row>
