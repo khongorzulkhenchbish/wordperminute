fe6e03506df564a13bf7a51172195c800aa98cb0
Khongorzul Khenchbish <lolhongorzul@gmail.com>
2025-09-26 21:50:50 +0200

    redesigned the pop-up window

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,27 +1,9 @@
 import React from "react";
-import {Container, Button } from "react-bootstrap";
+import {Container, Button, Form, InputGroup, Row, Col } from "react-bootstrap";
 import "../styles/Layout.css";
-import Poor from './pop-up/Poor';
-import Avg from './pop-up/Average';
-import Excellent from './pop-up/Excellent';
 import data from "../resources/data.json";
 
 const PopUp = (props) => {
-    const showPopUp = () => {
-        const score = props.data.correct;
-        const accuracy = props.data.accuracy;
-        const correct = props.data.correct;
-
-        if(score < 28) {
-            return Poor(accuracy, correct);
-        }
-        else if(score < 42) {
-            return Avg(accuracy, correct);
-        } 
-        else {
-            return Excellent(accuracy, correct);
-        }
-    }
     const refreshPage = () => {
         window.location.reload(false);
     }
@@ -30,9 +12,35 @@ const PopUp = (props) => {
           <div className="popup-overlay">
             <div className="popup-wrapper">
                 <div className="popup-window">
-                    {showPopUp()}
+                    <div className="popup-header">{data.final_result}</div>
+                    <Container className="popup-body">
+                        <Row className="justify-content-md-center" style={{marginBottom: '30px', marginTop: '30px'}}>
+                            <Col className="meter center-content">
+                                <Col id="wordMin">
+                                    {props.data.correct}
+                                </Col>
+                                <Col className="desc">{data.wrdMin} </Col>
+                            </Col>
+                            <Col className="meter center-content">
+                                <Col id="accuracy">
+                                    {props.data.accuracy}%
+                                </Col>
+                                <Col className="desc">{data.accuracy} </Col>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                        placeholder="Enter your username"
+                        aria-label="Enter your username"
+                        aria-describedby="basic-addon2"
+                        />
+                        <Button variant="outline-secondary" id="button-addon2" className="contactButton">
+                        Save Score
+                        </Button>
+                    </InputGroup>
                     <div className="popup-footer">
-                        <Button variant="primary" onClick={refreshPage}>{data.btn_restart}</Button>{' '}
+                        <Button className="contactButton" onClick={refreshPage}>{data.btn_restart}</Button>{' '}
                     </div>
                 </div>
             </div>
