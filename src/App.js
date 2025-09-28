f56d8bed09d0bca40afc238d2051747b8ba6f1b0
Khongorzul Khenchbish <lolhongorzul@gmail.com>
2025-09-28 19:42:42 +0200

    feature: dark mode

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Scoreboard from './pages/Scoreboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -10,7 +9,6 @@ function App() {
   return (
     <Switch>
       <Route path="/" exact component={Home}/>
-      <Route path="/scoreboard" exact component={Scoreboard}/>
     </Switch>
   );
 }
