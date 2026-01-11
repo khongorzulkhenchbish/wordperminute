c07875da8d90281ac5d2f811ae0bbd5cab4a7ee9
Khongorzul Khenchbish <lolhongorzul@gmail.com>
2026-01-11 22:47:41 +0100

    minor package update

--- src/components/Adv.js
+++ src/components/Adv.js
@@ -6,7 +6,7 @@ import '../styles/Layout.css'
 const Adv = () => {
   return (
     <Row className="adv">
-        <iframe className="content-box" src="https://www.youtube.com/embed/YQZEoZ4W0ac?si=Jq-g3tn0sD-cR0R_" title="YouTube video player"></iframe>
+        <iframe className="content-box" src="https://www.youtube.com/embed/JOglMvmJ3H0?si=2pN-fIZczqqXR6om" title="YouTube video player"></iframe>
         <div className="content-box">
         <h1>{data.content_header}</h1>
           <p>{data.adv_text}</p>
